3aa42481a3edad165aab2b7cbc6f2278514f6524
abhisheksenku <abhi971255@gmail.com>
2026-05-10 14:40:27 +0530

    consent in route error

--- backend/routes/consentRoutes.js
+++ backend/routes/consentRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { saveConsent } = require('../controllers/consentController');
+const { saveConsent, getConsentStatus } = require('../controllers/consentController');
 
 const { authMiddleware } = require('../middleware/authMiddleware');
 
